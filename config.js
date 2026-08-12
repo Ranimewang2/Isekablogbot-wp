8d4bfd1fdde8b36fa693a91b524213a8b0e75174
Ranimewang2 <rr2667431@gmail.com>
2026-08-12 16:56:04 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 const config = {
   // ── WordPress ──────────────────────────────────────────
   wpUrl:         'https://isekaiblogging.gt.tc',
-  wpUsername:    'isekai',
-  wpAppPassword: '4KlY fX22 imD! cFlV &lWB VZCz',  // WP App Password (spaces are fine)
+  wpUsername:    'rahatxashito',
+  wpAppPassword: 'ashito6969',  // WP App Password (spaces are fine)
 
   // ── OpenRouter ─────────────────────────────────────────
   openrouterKey: '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',
